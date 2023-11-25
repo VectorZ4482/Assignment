8c1b85a2097f5b1c9d5a47f5690a95a524ca9ec2
VectorZ <aptechmoin2241@gmail.com>
2023-11-25 14:48:39 +0500

    Project

---
+++ zoom.js
@@ -0,0 +1,188 @@
+
+            function zoomImage() {
+               
+                var imageUrl = '/images/c1.png';
+                
+    
+             
+                var newWindow = window.open(imageUrl, 'ProductZoom', 'width=1000, height=700');
+                
+            
+                newWindow.focus();
+            }
+        
+
+            
+            function zoomImage2() {
+               
+                var imageUrl2 = '/images/c2.png';
+                
+    
+             
+                var newWindow = window.open(imageUrl2, 'ProductZoom', 'width=1000, height=700');
+                
+            
+                newWindow.focus();
+            }
+        
+            
+            function zoomImage3() {
+               
+                var imageUrl3 = '/images/c3.png';
+                
+    
+             
+                var newWindow = window.open(imageUrl3, 'ProductZoom', 'width=1000, height=700');
+                
+            
+                newWindow.focus();
+            }
+            function zoomImage4() {
+               
+                var imageUrl4 = '/images/c4.png';
+                
+    
+             
+                var newWindow = window.open(imageUrl4, 'ProductZoom', 'width=1000, height=700');
+                
+            
+                newWindow.focus();
+            }
+
+            function zoomImage5() {
+               
+                var imageUrl5 = '/images/c5.png';
+                
+    
+             
+                var newWindow = window.open(imageUrl5, 'ProductZoom', 'width=1000, height=700');
+                
+            
+                newWindow.focus();
+            }
+
+            function zoomImage6() {
+               
+                var imageUrl6 = '/images/c6.png';
+                
+    
+             
+                var newWindow = window.open(imageUrl6, 'ProductZoom', 'width=1000, height=700');
+                
+            
+                newWindow.focus();
+            }
+
+            function zoomImage7() {
+               
+                var imageUrl7 = '/images/c7.png';
+                
+    
+             
+                var newWindow = window.open(imageUrl7, 'ProductZoom', 'width=1000, height=700');
+                
+            
+                newWindow.focus();
+            }
+
+            function zoomImage8() {
+               
+                var imageUrl8 = '/images/c8.png';
+                
+    
+             
+                var newWindow = window.open(imageUrl8, 'ProductZoom', 'width=1000, height=700');
+                
+            
+                newWindow.focus();
+            }
+        
+            function zoomImage9() {
+               
+                var imageUrl9 = '/images/c9.png';
+                
+    
+             
+                var newWindow = window.open(imageUrl9, 'ProductZoom', 'width=1000, height=700');
+                
+            
+                newWindow.focus();
+            }
+            function zoomImage10() {
+               
+                var imageUrl10 = '/images/c10.png';
+                
+    
+             
+                var newWindow = window.open(imageUrl10, 'ProductZoom', 'width=1000, height=700');
+                
+            
+                newWindow.focus();
+            }
+            function zoomImage11() {
+               
+                var imageUrl11 = '/images/c11.png';
+                
+    
+             
+                var newWindow = window.open(imageUrl11, 'ProductZoom', 'width=1000, height=700');
+                
+            
+                newWindow.focus();
+            }
+            function zoomImage12() {
+               
+                var imageUrl12 = '/images/c12.png';
+                
+    
+             
+                var newWindow = window.open(imageUrl12, 'ProductZoom', 'width=1000, height=700');
+                
+            
+                newWindow.focus();
+            }
+
+            function zoomImage13() {
+               
+                var imageUrl13 = '/images/a1.png';
+                
+    
+             
+                var newWindow = window.open(imageUrl13, 'ProductZoom', 'width=1000, height=700');
+                
+            
+                newWindow.focus();
+            }
+            function zoomImage14() {
+               
+                var imageUrl14 = '/images/a2.png';
+                
+    
+             
+                var newWindow = window.open(imageUrl14, 'ProductZoom', 'width=1000, height=700');
+                
+            
+                newWindow.focus();
+            }
+            function zoomImage15() {
+               
+                var imageUrl15 = '/images/a3.png';
+                
+    
+             
+                var newWindow = window.open(imageUrl15, 'ProductZoom', 'width=1000, height=700');
+                
+            
+                newWindow.focus();
+            }
+            function zoomImage16() {
+               
+                var imageUrl16 = '/images/a4.png';
+                
+    
+             
+                var newWindow = window.open(imageUrl16, 'ProductZoom', 'width=1000, height=700');
+                
+            
+                newWindow.focus();
+            }
